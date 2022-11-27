958536a63d796024c91a330806fe040c81b8e6a3
tmharwood <105583481+tmharwood@users.noreply.github.com>
2022-11-26 16:02:41 -0800

    done
    i think, pending clarification on Ed

--- client.py
+++ client.py
@@ -11,32 +11,36 @@ PORT = 51055  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    msg = input(">")
-    total_sent = 0
-    # Code to convert int to bytes adapted from: https://www.geeksforgeeks.org/how-to-convert-int-to-bytes-in-python/
-    msg_len = len(msg)
-    s.send(msg_len.to_bytes(3, 'big'))
-    # send message to server
-    while total_sent < msg_len:
-        sent = s.send(msg[total_sent:].encode())
-        if sent == 0:
-            raise RuntimeError("!!!CLIENT!!! Connection broken.")
-        total_sent = total_sent + sent
-    
-    # receive message from server
-    chunks = []
-    bytes_rec = 0
-    data_len = s.recv(1024)
-    data_len = int.from_bytes(data_len, "big")
-    print("!!!CLIENT!!! length of message from server: ", data_len)
-    while bytes_rec < data_len:
-        chunk = s.recv(min(data_len - bytes_rec, 2048))
-        if chunk == b"":
-            raise RuntimeError("sockey broken")
-        chunks.append(chunk)
-        bytes_rec += len(chunk)
-    
-    print(b''.join(chunks).decode("utf-8"))
+    while True:
+        msg = input(">")
+        total_sent = 0
+        # Code to convert int to bytes adapted from: https://www.geeksforgeeks.org/how-to-convert-int-to-bytes-in-python/
+        msg_len = len(msg)
+        s.send(msg_len.to_bytes(3, 'big'))
+        # send message to server
+        while total_sent < msg_len:
+            sent = s.send(msg[total_sent:].encode())
+            if sent == 0:
+                raise RuntimeError("!!!CLIENT!!! Connection broken.")
+            total_sent = total_sent + sent
+        #print(msg)
+        if msg == "/q":
+            break
+        # receive message from server
+        chunks = []
+        bytes_rec = 0
+        data_len = s.recv(1024)
+        data_len = int.from_bytes(data_len, "big")
+        #print("!!!CLIENT!!! length of message from server: ", data_len)
+        while bytes_rec < data_len:
+            chunk = s.recv(min(data_len - bytes_rec, 2048))
+            if chunk == b"":
+                raise RuntimeError("socket broken")
+            chunks.append(chunk)
+            bytes_rec += len(chunk)
+        if chunks[0] == b"/q":
+            break
+        print(b''.join(chunks).decode("utf-8"))
 
 
 
--- server.py
+++ server.py
@@ -16,18 +16,45 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
+        #conn.setblocking(False)
         print(f"Connected by {addr}")
         while True:
             data_len = conn.recv(1024)
-            if not data_len or data_len == b"/q":
+            if data_len == b"/q":
                 break
             # code to convert from byte to int adapted from: https://www.geeksforgeeks.org/how-to-convert-bytes-to-int-in-python/
             data_len = int.from_bytes(data_len, "big")
-            print("!!!SERVER!!! length of msg rec: ", data_len)
-            data = conn.recv(2048)
-            if not data or data == b"/q":
+            #print("!!!SERVER!!! length of msg rec: ", data_len)
+
+            chunks = []
+            bytes_rec = 0
+            # Start receiving data in a loop
+
+            while bytes_rec < data_len:
+                chunk = conn.recv(min(data_len - bytes_rec, 2048))
+                if chunk == b"":
+                    raise RuntimeError("sockey broken")
+                chunks.append(chunk)
+                bytes_rec += len(chunk)
+            if chunks[0] == b"/q":
                 break
-            print("SERVER received: ", data)
-            conn.send(data_len.to_bytes(3, 'big'))
-            print("echo length: ", data_len)
-            conn.sendall(data)
+            print(b''.join(chunks).decode("utf-8"))
+
+            # Get input to send back to client
+            send_data = input(">")
+            send_len = len(send_data)
+            total_sent = 0
+
+            # Send message length
+            conn.send(send_len.to_bytes(3, 'big'))
+            #print("server msg len: ", send_len)
+            #conn.sendall(data)
+
+            # Start sending data in a loop
+            while total_sent < send_len:
+                sent = conn.send(send_data[total_sent:].encode())
+                if sent == 0:
+                    raise RuntimeError("!!!SERVER!!! Connection broken.")
+                total_sent = total_sent + sent
+            if send_data == "/q":
+                break
